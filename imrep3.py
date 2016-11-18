1161abebff8bc635a51552d3849bb8bd09637acd
Igor Mandric <mandric.igor@scientist.com>
2016-11-18 02:11:57 -0500

    clean-up

--- imrep3.py
+++ imrep3.py
@@ -4,6 +4,13 @@ import sys
 import argparse
 import os
 from collections import Counter
+import gzip
+
+try:
+    from StringIO import StringIO # Python 2
+except ImportError:
+    from io import StringIO # Python 3
+
 
 from Bio import SeqIO
 from intervaltree import IntervalTree
@@ -16,8 +23,7 @@ from utils import *
 
 
 
-
-
+cd = os.path.dirname(os.path.realpath(__file__))
 
 
 class Settings(object):
@@ -39,37 +45,17 @@ class ImReP(object):
         self._fastq_handle = None
 
         self.vi_pieces = {}
-        self.viset = set()
-        self.anchor_v = {}
-        self.v_tip = {}
-
         self.d_seqs = {}
-
-        self.jayset = set()
         self.jay_pieces = {}
-        self.anchor_j = {}
-        self.j_tip = {}
-
         self.pSeq_read_map = {}
-        self.read_vi_map = {}
-        self.read_jay_map = {}
-
         self.just_v = []
         self.just_j = []
-        #------------------------
-
-
-        self.vis = {}
-        self.jays = {}
-
         self.hashV = {}
         self.hashJ = {}
-
         self.v_chain_type = {}
         self.j_chain_type = {}
-
         self.__populate_v()
-        #self.__populate_d()
+        self.__populate_d()
         self.__populate_j()
         self.__read_reads()
 
@@ -83,10 +69,9 @@ class ImReP(object):
 
 
     def __populate_v(self):
-        chains_v = ["db/IGHV.faa", "db/IGKV.faa", "db/IGLV.faa", 
-                    "db/TRAV.faa", "db/TRBV.faa", "db/TRDV.faa", "db/TRGV.faa"]
+        global cd
+        chains_v = map(lambda x: cd + "/db/%sV.faa" % x, self.__settings.chains)
         for ch_v_file in chains_v:
-            #chain_name = ch_v_file.split("/")[-1].split(".")[0][:-1]
             for record in SeqIO.parse(ch_v_file, "fasta"):
                 if "partial in 3'" not in record.description:
                     Vend = record.seq.tostring()[-20:]
@@ -95,24 +80,27 @@ class ImReP(object):
                         if k not in self.hashV:
                             self.hashV[k] = set()
                         self.hashV[k].add(record.id)
-                    self.v_chain_type[record.id] = getGeneType2(record.id)#chain_name
+                    self.v_chain_type[record.id] = getGeneType2(record.id)
                     posC = Vend.rfind("C")
                     if posC != -1:
                         anchor = Vend[:posC]
                         rest = Vend[posC + 1:]
                         self.vi_pieces[record.id] = (anchor, rest)
-        #print len(self.hashV)
 
-    #def __populate_d(self):
-    #    for record in SeqIO.parse("db/IGHD.faa", "fasta"):
-    #        self.d_seqs[record.id] = record.seq.tostring()
+    def __populate_d(self):
+        global cd
+        for chain in self.__settings.chains:
+            if chain in ["IGH", "TRB", "TRD"]:
+                for record in SeqIO.parse(cd + "/db/%sD.faa" % chain, "fasta"):
+                    if chain not in self.d_seqs:
+                        self.d_seqs[chain] = {}
+                    self.d_seqs[chain][record.id] = record.seq.tostring()
 
 
     def __populate_j(self):
-        chains_j = ["db/IGHJ.faa", "db/IGKJ.faa", "db/IGLJ.faa", 
-                    "db/TRAJ.faa", "db/TRBJ.faa", "db/TRDJ.faa", "db/TRGJ.faa"]
+        global cd
+        chains_j = map(lambda x: cd + "/db/%sJ.faa" % x, self.__settings.chains)
         for ch_j_file in chains_j:
-            #chain_name = ch_j_file.split("/")[-1].split(".")[0][:-1]
             for record in SeqIO.parse(ch_j_file, "fasta"):
                 beginJ = record.seq.tostring()[:20]
                 kmrs = self.kmers(beginJ, 3)
@@ -120,7 +108,7 @@ class ImReP(object):
                     if k not in self.hashJ:
                         self.hashJ[k] = set()
                     self.hashJ[k].add(record.id)
-                self.j_chain_type[record.id] = getGeneType2(record.id)#chain_name
+                self.j_chain_type[record.id] = getGeneType2(record.id)
                 letter = "F"
                 if "IGHJ" in ch_j_file:
                     letter = "W"
@@ -129,13 +117,17 @@ class ImReP(object):
                     anchor = beginJ[:posW]
                     rest = beginJ[posW + 1:]
                     self.jay_pieces[record.id] = (anchor, rest)
-        #print len(self.hashJ)
 
 
 
     def __read_reads(self):
         fastqfile = self.__settings.fastqfile
-        self._fastq_handle = SeqIO.parse(fastqfile, "fasta")
+        if fastqfile.endswith(".gz"):
+            with gzip.open(fastqfile, 'rb') as f:
+                file_content = f.read()
+            self._fastq_handle = SeqIO.parse(StringIO(file_content), "fasta")
+        else:
+            self._fastq_handle = SeqIO.parse(fastqfile, "fasta")
 
 
     def __full_cdr3(self):
@@ -146,7 +138,6 @@ class ImReP(object):
         full_cdr3 = []
         for record in self._fastq_handle:
             pSequences = nucleotide2protein2(str(record.seq))
-            #pSequences = ["CQQYYSYSTF"]
             if pSequences:
                 for pSeq in pSequences:
                     pos1 = pSeq.find("C")
@@ -160,8 +151,6 @@ class ImReP(object):
                         for v in interV:
                             vlist.extend(list(self.hashV[v]))
                         if vlist:
-                            #vc = max(Counter(vlist).items(), key=lambda z: z[1])[1]
-                            #vc = [x for x, y in Counter(vlist).items() if y == vc]
                             vc = [x for x, y in Counter(vlist).items()]
                         else:
                             vc = []
@@ -186,14 +175,8 @@ class ImReP(object):
                                     mismatch2 = jellyfish.levenshtein_distance(unicode(s[:minlen2]), unicode(vv[:minlen2]))
                                 else:
                                     mismatch2 = 0
-                                #print minlen1, minlen2, mismatch1, mismatch2, pSeq, v1, record.seq.tostring()
                                 if (minlen1 == 0 and mismatch2 <= 1) or (minlen1 > 3 and mismatch1 <= 1 and minlen2 >= 2 and mismatch2 <= 2):
                                     vtypes[v3] = mismatch1 + mismatch2
-                                    #print minlen1, minlen2, mismatch1, mismatch2
-                                #if (minlen1 + minlen2 >= 4 and mismatch1 + mismatch2 <= 2):
-                                #    vtypes[v3] = mismatch1 + mismatch2
-
-                    #print vtypes, "VTYPES"
                     if pos2 != [-1, -1]:
                         if pos2[0] != -1:
                             if pos2[1] > 10:
@@ -206,8 +189,6 @@ class ImReP(object):
                             for j in interJ:
                                 jlist.extend(list(self.hashJ[j]))
                             if jlist:
-                                #jc = max(Counter(jlist).items(), key=lambda z: z[1])[1]
-                                #jc = [x for x, y in Counter(jlist).items() if y == jc]
                                 jc = [x for x, y in Counter(jlist).items()]
                             else:
                                 jc = []
@@ -233,21 +214,8 @@ class ImReP(object):
                                         mismatch1 = jellyfish.levenshtein_distance(unicode(f[-minlen1:]), unicode(j[-minlen1:]))
                                     else:
                                         mismatch1 = 0
-
-                                    #if (minlen1 + minlen2 >= 4 and mismatch1 + mismatch2 <= 2):
-                                    #    jtypes[j3] = mismatch1 + mismatch2
-                                    #print (minlen1, minlen2, mismatch1, mismatch2), j2
                                     if (minlen2 == 0 and mismatch1 <= 1) or (minlen2 > 3 and mismatch2 <= 1 and minlen1 >= 2 and mismatch1 <= 2):
                                         jtypes[j3] = mismatch1 + mismatch2
-
-                                    """
-                                    if (minlen2 == 0 and mismatch1 <= 1):
-                                        jcred[j3] = (minlen1, minlen2, mismatch1, mismatch2)
-                                    elif (minlen2 > 0 and mismatch2 <= 1 and minlen1 > 0 and mismatch1 <= 2):
-                                        jcred[j3] = (minlen1, minlen2, mismatch1, mismatch2)
-                                    """
-
-
                         if pos2[1] != -1:
                             if pos2[1] > 10:
                                 offset = pos2[1] - 10
@@ -259,8 +227,6 @@ class ImReP(object):
                             for j in interJ:
                                 jlist.extend(list(self.hashJ[j]))
                             if jlist:
-                                #jc = max(Counter(jlist).items(), key=lambda z: z[1])[1]
-                                #jc = [x for x, y in Counter(jlist).items() if y == jc]
                                 jc = [x for x, y in Counter(jlist).items()]
                             else:
                                 jc = []
@@ -286,14 +252,8 @@ class ImReP(object):
                                         mismatch1 = jellyfish.levenshtein_distance(unicode(f[-minlen1:]), unicode(j[-minlen1:]))
                                     else:
                                         mismatch1 = 0
-                                    #print minlen1, minlen2, mismatch1, mismatch2, unicode(s[:minlen2]), unicode(jj[:minlen2]), unicode(f[-minlen1:]), unicode(j[-minlen1:]), j, jj
-                                    #if (minlen1 + minlen2 >= 4 and mismatch1 + mismatch2 <= 2):
-                                    #    jtypes[j3] = mismatch1 + mismatch2
                                     if (minlen2 == 0 and mismatch1 <= 1) or (minlen2 > 3 and mismatch2 <= 1 and minlen1 >= 2 and mismatch1 <= 2):
                                         jtypes[j3] = mismatch1 + mismatch2
-
-
-                    #print vtypes, jtypes, pSeq
                     if vtypes or jtypes:
                         vt = {}
                         jt = {}
@@ -307,19 +267,14 @@ class ImReP(object):
                             if chaint[:3] not in jt:
                                 jt[chaint[:3]] = []
                             jt[chaint[:3]].append(x)
-
-                        #print vtypes, vt.keys(), jt.keys(), pSeq, record.seq.tostring()
                         common = set(vt.keys()) & set(jt.keys())
                         if common:
-                            #print "COMMON", common, pSeq, record.seq.tostring()
                             if "IGH" in common:
                                 full_cdr3.append(pSeq[pos1: pos2[1] + 1])
                                 cdr3 = pSeq[pos1: pos2[1] + 1]
                             else:
                                 full_cdr3.append(pSeq[pos1: pos2[0] + 1])
                                 cdr3 = pSeq[pos1: pos2[0] + 1]
-                            #if cdr3 == "CTTEVTLVDTAMVTRHKNYYYYYYMDVW":
-                            #    print "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
                             if cdr3 not in self.pSeq_read_map or (cdr3 in self.pSeq_read_map and ("v" not in self.pSeq_read_map[cdr3].keys() or "j" not in self.pSeq_read_map[cdr3].keys())):
                                 v_t = []
                                 j_t = []
@@ -337,16 +292,11 @@ class ImReP(object):
                                             chtype[key] = []
                                         chtype[key].extend(ch)
                                 self.pSeq_read_map[cdr3] = {"v": map(getGeneType, v_t), "j": map(getGeneType, j_t), "chain_type": chtype}
-                            #print self.pSeq_read_map[cdr3], "OOOOOOOOOO"
-
                         elif vtypes and not jtypes:
                             vi_partial = pSeq[pos1:]
-                            #if vi_partial == "CTTEVTLVDTAMVTRHKNYYYYYYMDVW":
-                            #    print pSeq, "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
                             if vi_partial not in full_cdr3:
                                 self.just_v.append(vi_partial)
                             if vi_partial not in self.pSeq_read_map and vi_partial not in full_cdr3:
-                                #print {"v": map(getGeneType, vtypes), "chain_type": vt}
                                 self.pSeq_read_map[vi_partial] = {"v": map(getGeneType, vtypes), "chain_type": vt}
                         elif jtypes and not vtypes:
                             if "IGH" in jt:
@@ -355,67 +305,8 @@ class ImReP(object):
                                 jay_partial = pSeq[:pos2[0] + 1]
                             if jay_partial not in full_cdr3:
                                 self.just_j.append(jay_partial)
-                            #if jay_partial in self.just_v:
-                            #    print "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM", jt, common, jay_partial, pSeq, self.pSeq_read_map[jay_partial], jtypes
-                            #if jay_partial == "CTTEVTLVDTAMVTRHKNYYYYYYMDVW":
-                            #    print pSeq, jt, jtypes, pos1, pos2
                             if jay_partial not in self.pSeq_read_map and jay_partial not in full_cdr3:
                                 self.pSeq_read_map[jay_partial] = {"j": map(getGeneType, jtypes), "chain_type": jt}
-
-
-
-
-                    """
-                    if vtypes:
-                        best_match_V = min(vtypes.items(), key=lambda z: z[1])
-                        best_match_V = [x for x, y in vtypes.items() if y == best_match_V[1]]
-                    else:
-                        best_match_V = []
-
-                    if jtypes:
-                        best_match_J = min(jtypes.items(), key=lambda z: z[1])
-                        best_match_J = [x for x, y in jtypes.items() if y == best_match_J[1]]
-                    else:
-                        best_match_J = []
-
-                    if best_match_V or best_match_J:
-                        if best_match_V:
-                            best_v_types = set(map(lambda z: self.v_chain_type[z], best_match_V))
-                        else:
-                            best_v_types = set()
-                        if best_match_J:
-                            best_j_types = set(map(lambda z: self.j_chain_type[z], best_match_J))
-                        else:
-                            best_j_types = set()
-
-
-
-                        #print best_match_V, best_match_J, best_v_types, best_j_types
-
-                        if len(best_v_types) == 1 and len(best_j_types) == 1 and list(best_v_types)[0] == list(best_j_types)[0]:
-                            if list(best_v_types)[0] == "IGHV":
-                                full_cdr3.append(pSeq[pos1: pos2[0]])
-                                cdr3 = pSeq[pos1: pos2[0] + 1]
-                            else:
-                                full_cdr3.append(pSeq[pos1: pos2[1]])
-                                cdr3 = pSeq[pos1: pos2[1] + 1]
-                            if cdr3 not in self.pSeq_read_map:
-                                self.pSeq_read_map[cdr3] = {"v": map(getGeneType, best_match_V), "j": map(getGeneType, best_match_J), "chain_type": list(best_j_types)[0][:3]}
-                        elif best_v_types and not best_j_types:
-                            vi_partial = pSeq[pos1:]
-                            self.just_v.append(vi_partial)
-                            if vi_partial not in self.pSeq_read_map:
-                                self.pSeq_read_map[vi_partial] = {"v": map(getGeneType, best_match_V), "chain_type": list(best_v_types)[0][:3]}
-                        elif best_j_types and not best_v_types:
-                            if list(best_j_types)[0] == "IGH":
-                                jay_partial = pSeq[:pos2[0] + 1]
-                            else:
-                                jay_partial = pSeq[:pos2[1] + 1]
-                            self.just_j.append(jay_partial)
-                            if jay_partial not in self.pSeq_read_map:
-                                self.pSeq_read_map[jay_partial] = {"j": map(getGeneType, best_match_J), "chain_type": list(best_j_types)[0][:3]}
-                    """
-
         return full_cdr3
 
 
@@ -463,18 +354,17 @@ class ImReP(object):
                     countVJ = min(countV, countJ)
                     for x in range(countVJ):
                         handshakes.append(newly_born_cdr3)
-                    #if newly_born_cdr3 not in self.pSeq_read_map:
-                    #self.pSeq_read_map[newly_born_cdr3] = {"v": self.pSeq_read_map[list(overlapping_v)[0].data]["v"],
-                    #                                       "j": self.pSeq_read_map[j]["j"], "chain_type": self.pSeq_read_map[j]["chain_type"]}
                     self.pSeq_read_map[newly_born_cdr3] = {"v": v_t, "j": j_t, "chain_type": chtype}
         return handshakes
 
 
-    def __map_j(self, seq):
+    def __map_d(self, seq, chain_type):
         d_types = set()
-        for d_t, d_seq in self.d_seqs.items():
+        for d_t, d_seq in self.d_seqs[chain_type].items():
             if seq.find(d_seq) != -1:
                 d_types.add(getGeneType(d_t))
+        if not d_types:
+            return set(["NA"])
         return d_types
 
 
@@ -486,14 +376,16 @@ class ImReP(object):
         clones = Counter(clones)
         cast_clustering = Cast(clones)
         clustered_clones = cast_clustering.doCast(self.__settings.castThreshold)
-        #print "MMMM", self.pSeq_read_map
         for clone in clustered_clones:
-            #j_types = self.__map_j(clone[0])
-            #print clone
-            if True:#if j_types:
-                clone.extend([",".join(set(self.pSeq_read_map[clone[0]]["v"])),
-                              #",".join(j_types),
-                              ",".join(set(self.pSeq_read_map[clone[0]]["j"]))])
+            chain_type = self.pSeq_read_map[clone[0]]["v"][0][:3]
+            j_types = None
+            if chain_type in ["IGH", "TRB", "TRD"]:
+                j_types = self.__map_d(clone[0], chain_type)
+            types = [",".join(set(self.pSeq_read_map[clone[0]]["v"]))]
+            if j_types:
+                types.append(",".join(j_types))
+            types.append(",".join(set(self.pSeq_read_map[clone[0]]["j"])))
+            clone.extend(types)
         return clustered_clones
 
 
@@ -509,7 +401,8 @@ if __name__ == "__main__":
     optional_arguments = ap.add_argument_group("Optional Inputs")
     optional_arguments.add_argument("-o", "--overlapLen", help="overlap length between v and j", type=int)
     optional_arguments.add_argument("--noOverlapStep", help="whether to execute overlap step with suffix trees", dest="noOverlapStep", action="store_true")
-    optional_arguments.add_argument("-c", "--castThreshold", help="threshold for CAST clustering algorithm", type=float)
+    optional_arguments.add_argument("-t", "--castThreshold", help="threshold for CAST clustering algorithm", type=float)
+    optional_arguments.add_argument("-c", "--chains", help="chains: comma separated values from IGH,IGK,IGL,TRA,TRB,TRD,TRG", type=str)
 
     args = ap.parse_args()
 
@@ -521,6 +414,7 @@ if __name__ == "__main__":
         'overlapLen': 10,
         'noOverlapStep': False,
         'castThreshold': 0.2,
+        'chains': ['IGH','IGK','IGL','TRA','TRB','TRD','TRG']
     }
 
     if args.overlapLen:
@@ -529,6 +423,8 @@ if __name__ == "__main__":
         set_dict["noOverlapStep"] = args.noOverlapStep
     if args.castThreshold:
         set_dict["castThreshold"] = args.castThreshold
+    if args.chains:
+        set_dict["chains"] = args.chains.split(",")
 
     settings = Settings(**set_dict)
 
