50cd9d874d19691fcd36fbd278af760caea984a1
Serghei Mangul <serghei@login1.(none)>
2017-07-19 11:58:59 -0700

    prepare for release v0.7

---
+++ clonality.py
@@ -0,0 +1,205 @@
+import argparse
+import csv
+import sys
+from collections import Counter
+from math import log as ln
+import os
+from os.path import basename
+
+
+def p(n, N):
+    """ Relative abundance """
+    if n is  0:
+        return 0
+    else:
+        return (float(n)/N) * ln(float(n)/N)
+
+def sdi(data):
+    N = sum(data.values())
+    
+    return -sum(p(n, N) for n in data.values() if n is not 0)
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('inFile', help='Mapped reads in bam format')
+ap.add_argument('outDir', help='Mapped reads in bam format')
+
+
+args = ap.parse_args()
+
+if not os.path.exists(args.outDir):
+    os.makedirs(args.outDir)
+
+
+#------
+print "Read CDR3 assembled by ImreP", args.inFile
+
+#HWI-ST1148:152:C3LK5ACXX:7:2301:14697:52825/1/1 CALPIFHSRIRRRPLRPILHSRIHKHYYNKHPHHYNLPRKIVVGIVF TRAV9,TRAV18,TRAV11     NA      TRAJ4   TRAV9-2*01:4:1,TRAV9-1*01:4:1,TRAV11*01:3:0,TRAV18*01:4:1,TRAV6*01:4:1,TRAV16*01:4:1    NA      0       0
+
+
+#CDR3_AA_Seq     Read_count      V_chains        D_chains        J_chains
+#CALPISGTRASKLFGLAATRVSYQQGPVILDEDVFDLHLGSLIHIFLVIGLQGF  2       TRAV18,TRAV19,TRAV11    NA      TRAJ25,TRAJ35
+
+
+cdr3_IGH={}
+cdr3_IGK={}
+cdr3_IGL={}
+
+cdr3_TCRA={}
+cdr3_TCRB={}
+cdr3_TCRD={}
+cdr3_TCRG={}
+
+
+
+
+
+base=os.path.splitext(basename(args.inFile))[0]
+
+
+with open(args.inFile) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter='\t')
+    next(readCSV)
+    for row in readCSV:
+        V=row[2]
+        J=row[4]
+        cdr3=row[0]
+        count=int(row[2])
+        
+        
+        if row!=[]:
+            
+            
+            if "*" not in row[0]:
+                if row[1]=="IGH":
+                        cdr3_IGH[cdr3]=count
+                elif row[1]=="IGK":
+                        cdr3_IGK[cdr3]=count
+                elif row[1]=="IGL":
+                        cdr3_IGL[cdr3]=count
+                elif row[1]=="TRA":
+                    cdr3_TCRA[cdr3]=count
+                elif row[1]=="TRB":
+                    cdr3_TCRB[cdr3]=count
+                elif row[1]=="TRG":
+                    cdr3_TCRG[cdr3]=count
+                elif row[1]=="TRD":
+                    cdr3_TCRD[cdr3]=count
+                else:
+                    print "ERROR : ",row, V,J
+                    sys.exit(1)
+
+
+
+
+fileSTAT=open(args.outDir+"/SUMMARY_"+args.outDir+".txt","w")
+
+
+fileSTAT.write("SAMPLE,nIGH,nIGK,nIGL,nTCRA,nTCRB,nTCRD,nTCRG, loadIGH,nIGK,loadIGL,loadTCRA,loadTCRB,loadTCRD,loadTCRG,alphaIGH,alphaIGK,alphaIGL,alphaTCRA,alphaTCRB,alphaTCRD,alphaTCRG")
+fileSTAT.write("\n")
+
+
+
+
+nIGH=str(len((cdr3_IGH)))
+nIGK=str(len((cdr3_IGK)))
+nIGL=str(len((cdr3_IGL)))
+nTCRA=str(len((cdr3_TCRA)))
+nTCRB=str(len((cdr3_TCRB)))
+nTCRD=str(len((cdr3_TCRD)))
+nTCRG=str(len((cdr3_TCRG)))
+
+loadIGH=str(sum(cdr3_IGH.values()))
+loadIGK=str(sum(cdr3_IGK.values()))
+loadIGL=str(sum(cdr3_IGL.values()))
+loadTCRA=str(sum(cdr3_TCRA.values()))
+loadTCRB=str(sum(cdr3_TCRB.values()))
+loadTCRD=str(sum(cdr3_TCRD.values()))
+loadTCRG=str(sum(cdr3_TCRG.values()))
+
+alphaIGH=str(sdi(Counter(cdr3_IGH)))
+alphaIGK=str(sdi(Counter(cdr3_IGK)))
+alphaIGL=str(sdi(Counter(cdr3_IGL)))
+alphaTCRA=str(sdi(Counter(cdr3_TCRA)))
+alphaTCRB=str(sdi(Counter(cdr3_TCRB)))
+alphaTCRD=str(sdi(Counter(cdr3_TCRD)))
+alphaTCRG=str(sdi(Counter(cdr3_TCRG)))
+
+
+              
+fileSTAT.write(base+","+nIGH+","+nIGK+","+nIGL+","+nTCRA+","+nTCRB+","+nTCRD+","+nTCRG+","+loadIGH+","+loadIGK+","+loadIGL+","+loadTCRA+","+loadTCRB+","+loadTCRD+","+loadTCRG+","+alphaIGH+","+alphaIGK+","+alphaIGL+","+alphaTCRA+","+alphaTCRB+","+alphaTCRD+","+alphaTCRG)
+fileSTAT.write("\n")
+
+print "Total number of IGH CDR3 is", len(set(cdr3_IGH))
+print "Total number of IGK CDR3 is", len(set(cdr3_IGK))
+print "Total number of IGL CDR3 is", len(set(cdr3_IGL))
+print "Total number of TCRA CDR3 is", len(set(cdr3_TCRA))
+print "Total number of TCRB CDR3 is", len(set(cdr3_TCRB))
+print "Total number of TCRG CDR3 is", len(set(cdr3_TCRG))
+print "Total number of TCRD CDR3 is", len(set(cdr3_TCRD))
+
+
+
+
+#IGH
+fileIGH=open(args.outDir+"/IGH_cdr3_"+args.outDir+".txt","w")
+#fileIGH.write("CDR3,nReads")
+#fileIGH.write("\n")
+for key in  sorted(cdr3_IGH):
+    fileIGH.write(key)
+    fileIGH.write("\n")
+
+
+#IGK
+fileIGK=open(args.outDir+"/IGK_cdr3_"+args.outDir+".txt","w")
+#fileIGK.write("CDR3,nReads")
+#fileIGK.write("\n")
+for key in  sorted(cdr3_IGK):
+    fileIGK.write(key)
+    fileIGK.write("\n")
+
+#IGL
+fileIGL=open(args.outDir+"/IGL_cdr3_"+args.outDir+".txt","w")
+#fileIGL.write("CDR3,nReads")
+#fileIGL.write("\n")
+for key in  sorted(cdr3_IGL):
+    fileIGL.write(key)
+    fileIGL.write("\n")
+
+
+#TCRA
+fileTCRA=open(args.outDir+"/TCRA_cdr3_"+args.outDir+".txt","w")
+#fileTCRA.write("CDR3,nReads")
+#fileTCRA.write("\n")
+for key in  sorted(cdr3_TCRA):
+    fileTCRA.write(key)
+    fileTCRA.write("\n")
+
+#TCRB
+fileTCRB=open(args.outDir+"/TCRB_cdr3_"+args.outDir+".txt","w")
+#fileTCRB.write("CDR3,nReads")
+#fileTCRB.write("\n")
+for key in  sorted(cdr3_TCRB):
+    fileTCRB.write(key)
+    fileTCRB.write("\n")
+
+
+#TCRG
+fileTCRG=open(args.outDir+"/TCRG_cdr3_"+args.outDir+".txt","w")
+#fileTCRG.write("CDR3,nReads")
+#fileTCRG.write("\n")
+for key in  sorted(cdr3_TCRG):
+    fileTCRG.write(key)
+    fileTCRG.write("\n")
+
+#TCRD
+fileTCRD=open(args.outDir+"/TCRD_cdr3_"+args.outDir+".txt","w")
+#fileTCRD.write("CDR3,nReads")
+#fileTCRD.write("\n")
+for key in  sorted(cdr3_TCRD):
+    fileTCRD.write(key)
+    fileTCRD.write("\n")
+
+
+
+
--- imrep-bam.sh
+++ imrep-bam.sh
@@ -11,7 +11,12 @@ parser.add_argument('-r', '--release', default='hg19', type=str,
 help='For human we support hg19 (default) and hg38. For mouse we support only mm10 (default)')
 parser.add_argument('-chr', '--chr', default='', type=str,
 help='Use this option to specify the format of chromosome name in the bam file. By default it is : 1,2,3,...,X. In case the format is : chr1, chr2,..,chrX, use : -chr chr)')
+parser.add_argument('-c', '--chains', default='IGH,IGK,IGL,TRA,TRB,TRD,TRG', type=str,
+help='chains: comma separated values from IGH,IGK,IGL,TRA,TRB,TRD,TRG')
+
 EOF
+
+
 
 
 #echo ${SPECIES[@]}
@@ -22,6 +27,7 @@ EOF
 
 bam=$BAM
 
+
 basename=$(echo  ${bam##*/} | awk -F ".bam" '{print $1}')
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chr=${CHR[@]}
@@ -30,9 +36,22 @@ mkdir $basename
 
 samtools=${DIR}/ext/samtools
 
+
+if [ ! -f ${bam}.bai ]; then
+echo "Warning : Index of $bam not found"
+echo "Creating index ... (samtools index $bam)"
+
+samtools index $bam
+echo "DONE!"
+
+fi
+
+
+
 #--------------------------------------------------------------------------------------
 #UNMAPPED READS
 $samtools view -f 0x4 -bh  $bam | $samtools bam2fq - > ${basename}/${basename}_unmapped.fastq
+
 
 
 #--------------------------------------------------------------------------------------
@@ -53,25 +72,52 @@ $samtools view -f 0x4 -bh  $bam | $samtools bam2fq - > ${basename}/${basename}_u
 
 if [ ${SPECIES[@]} == "human" ]
 then
-echo "-----Human BCR and TCR annotations are used"
+echo "-->Human BCR and TCR annotations are used"
 
 
 if [ ${RELEASE[@]} == "hg19" ]
 then
-echo "-----Release hg19"
+echo "-->Release hg19"
 
 
+
+
+if [[ ${CHAINS[@]} == *"IGH"* ]]
+then
 $samtools view -bh ${bam} ${chr}14:106032614-107288051 | $samtools view -bh -F 4 - | $samtools bam2fq - >${basename}/${basename}_mapped_immune.fastq
+fi
+
+if [[ ${CHAINS[@]} == *"IGK"* ]]
+then
 $samtools view -bh ${bam} ${chr}2:89156874-89630436 | $samtools view -bh -F 4 - | $samtools bam2fq -  >>${basename}/${basename}_mapped_immune.fastq
+fi
+
+if [[ ${CHAINS[@]} == *"IGL"* ]]
+then
 $samtools view -bh ${bam} ${chr}22:22380474-23265085 | $samtools view -bh -F 4 - | $samtools bam2fq -  >>${basename}/${basename}_mapped_immune.fastq
+fi
+
+if [[ ${CHAINS[@]} == *"TRA"* ]]
+then
 $samtools view -bh ${bam} ${chr}14:22090057-23021075 | $samtools view -bh -F 4 - | $samtools bam2fq -  >>${basename}/${basename}_mapped_immune.fastq
+
+fi
+
+if [[ ${CHAINS[@]} == *"TRB"* ]]
+then
 $samtools view -bh ${bam} ${chr}7:141998851-142510972 | $samtools view -bh -F 4 - | $samtools bam2fq -    >>${basename}/${basename}_mapped_immune.fastq
+fi
+
+if [[ ${CHAINS[@]} == *"TRG"* ]]
+then
 $samtools view -bh ${bam} ${chr}7:38279625-38407656 | $samtools view -bh -F 4 - | $samtools bam2fq - >>${basename}/${basename}_mapped_immune.fastq
+fi
+
 fi
 
 if [ ${RELEASE[@]} == "hg38" ]
 then
-echo "-----Release hg38"
+echo "-->Release hg38"
 
 $samtools view -bh ${bam} ${chr}14:105586437-106879844 | $samtools view -bh -F 4 - | $samtools bam2fq - >${basename}/${basename}_mapped_immune.fastq
 $samtools view -bh ${bam} ${chr}2:88857361-90235368 | $samtools view -bh -F 4 - | $samtools bam2fq -  >>${basename}/${basename}_mapped_immune.fastq
@@ -83,14 +129,14 @@ fi
 
 
 else
-echo "-----Mouse(mm10) BCR and TCR annotations are used"
+echo "-->Mouse(mm10) BCR and TCR annotations are used"
 #igh="chr12:113258768-116009954"
 #igk="chr6:67555636-70726754"
 #igl="chr16:19026858-19260844"
 #tcra="chr14:52427967-54224198"
 #tcrb="chr6:40891296-41558371"
 #tcrg="chr13:19178042-19356476"
-
+#tcrd is inside tcra
 
 $samtools view -bh ${bam} ${chr}12:113258768-116009954 | $samtools view -bh -F 4 - | $samtools bam2fq - >${basename}/${basename}_mapped_immune.fastq
 $samtools view -bh ${bam} ${chr}6:67555636-70726754| $samtools view -bh -F 4 - | $samtools bam2fq -  >>${basename}/${basename}_mapped_immune.fastq
@@ -103,7 +149,15 @@ fi
 
 
 n=$(wc -l ${basename}/${basename}_mapped_immune.fastq | awk '{print $1/4}')
-echo "Number of reads extracted from BCR and TCR loci : "$n
+echo "Number of mapped reads extracted from BCR and TCR loci : "$n
+n_unm=$(wc -l ${basename}/${basename}_unmapped.fastq | awk '{print $1/4}')
+echo "Number of unmapped reads extracted: "$n_unm
+
+N=$((n + n_unm))
+
+echo "Merging mapped reads from BCR/TCR loci and unmapped reads ..."
+echo "In total $N reads were available for ImReP analysis"
+
 
 
 
@@ -113,7 +167,17 @@ cat ${basename}/${basename}_mapped_immune.fastq ${basename}/${basename}_unmapped
 rm ${basename}/${basename}_mapped_immune.fastq ${basename}/${basename}_unmapped.fastq
 
 
+if [ ${CHAINS[@]} == "IGH,IGK,IGL,TRA,TRB,TRD,TRG" ]
+then
+echo "Profile all chains : IGH,IGK,IGL,TRA,TRB,TRD,TRG"
 python ${DIR}/imrep.py --fastq ${basename}/${basename}_unmapped_plus_immune.fastq ${basename}/${basename}.cdr3
+else
+echo "Profile only chains :" ${CHAINS[@]}
+
+python ${DIR}/imrep.py -c ${CHAINS[@]} --fastq ${basename}/${basename}_unmapped_plus_immune.fastq ${basename}/${basename}.cdr3
+fi
+
+
 
 echo "Results are in ${basename}"
 echo "DONE!"
